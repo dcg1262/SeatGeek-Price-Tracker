eb832da72c4a5b49eaa1956be226ccfe55071e1a
dcg1262 <71304532+dcg1262@users.noreply.github.com>
2023-07-07 11:57:08 -0600

    Added multiple events feature

--- code_1.py
+++ code_1.py
@@ -4,36 +4,43 @@ import requests
 import config
 
 seatgeekClientId = config.api_key
-eventID = 5964238  # Dallas Cowboys vs New York Jets ID
-maxPrice = 100
-url = (
-    "https://api.seatgeek.com/2/events/"
-    + str(eventID)
-    + "?client_id="
-    + str(seatgeekClientId)
-)
-response = requests.get(url)
 
-data = response.json()
-eventDesc = (
-    data["title"]
-    + " (ID "
-    + str(eventID)
-    + ") at "
-    + data["venue"]["name"]
-    + " in "
-    + data["venue"]["city"]
-)
-print(eventDesc)
+events = {
+    1: {"eventid": 5964238, "maxprice": 100},  # Dallas Cowboys vs New York Jets
+    2: {"eventid": 5964241, "maxprice": 100},  # Dallas Cowboys vs Seattle Seahawks
+}
 
-lowestPrice = data["stats"]["lowest_price"]
+for event, event_info in events.items():
+    eventID = event_info["eventid"]  # Dallas Cowboys vs New York Jets ID
+    maxPrice = event_info["maxprice"]
+    url = (
+        "https://api.seatgeek.com/2/events/"
+        + str(eventID)
+        + "?client_id="
+        + str(seatgeekClientId)
+    )
+    response = requests.get(url)
 
-alertText = "Lowest price for " + eventDesc + ": $" + str(lowestPrice)
-alertText = alertText.replace(
-    "&", "%26"
-)  # & Messes with requests api, %26 is equivalent to &
-print(alertText)
+    data = response.json()
+    eventDesc = (
+        data["title"]
+        + " (ID "
+        + str(eventID)
+        + ") at "
+        + data["venue"]["name"]
+        + " in "
+        + data["venue"]["city"]
+    )
+    print(eventDesc)
 
-if lowestPrice <= maxPrice and lowestPrice != None:
-    url = f"https://api.telegram.org/bot{config.TOKEN}/sendMessage?chat_id={config.chat_id}&text={alertText}"
-    print(requests.get(url).json())  # this sends the message
+    lowestPrice = data["stats"]["lowest_price"]
+
+    alertText = "Lowest price for " + eventDesc + ": $" + str(lowestPrice)
+    alertText = alertText.replace(
+        "&", "%26"
+    )  # & Messes with requests api, %26 is equivalent to &
+    print(alertText)
+
+    if lowestPrice <= maxPrice and lowestPrice != None:
+        url = f"https://api.telegram.org/bot{config.TOKEN}/sendMessage?chat_id={config.chat_id}&text={alertText}"
+        print(requests.get(url).json())  # this sends the message
